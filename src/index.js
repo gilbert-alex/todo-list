9da7230d56299a02ce57789600aec051ba8aff5c
Alex Gilbert <118496831+gilbert-alex@users.noreply.github.com>
2024-08-17 14:04:56 -0700

    separate demo

--- src/index.js
+++ src/index.js
@@ -1,43 +1,17 @@
 import './style.css';
 
-import Todo from './modules/todo'
-import Project from './modules/project'
 import {initListeners} from './modules/screen-controller'
+import demo from './demo'
 
 export const projectList = [];
 export const filteredList = [];
 
 initListeners();
 
-// demo
+demo();
 
-const birdhouse = new Project('Build a birdhouse')
-const getReady = new Project('Get ready for day')
 
-const measure = new Todo(
-    'measure wood',
-    'measure twice cut once',
-    'tomorrow',
-    'high',
-);
 
-const cutWood = new Todo(
-    'cut wood',
-    'be sure you measured twice',
-    'tomorrow',
-    'medium',
-)
-
-const shower = new Todo(
-    'shower',
-    'can\'t smell bad',
-    'morning',
-    'high',
-)
-
-birdhouse.addTask(measure);
-birdhouse.addTask(cutWood);
-getReady.addTask(shower);
 
 
 import {updateScreen} from './modules/dom-util'
